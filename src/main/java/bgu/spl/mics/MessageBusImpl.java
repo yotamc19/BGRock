d9538ca077bd66b8a6cdbfb88df6e57543eacb20
Jotam Combe <jotamc@wix.com>
2024-12-23 18:50:17 +0200

    added all initial files

---
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -0,0 +1,62 @@
+package bgu.spl.mics;
+
+/**
+ * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
+ * Write your implementation here!
+ * Only one public method (in addition to getters which can be public solely for unit testing) may be added to this class
+ * All other methods and members you add the class must be private.
+ */
+public class MessageBusImpl implements MessageBus {
+	
+	@Override
+	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void complete(Event<T> e, T result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendBroadcast(Broadcast b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	@Override
+	public <T> Future<T> sendEvent(Event<T> e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void register(MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unregister(MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Message awaitMessage(MicroService m) throws InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
