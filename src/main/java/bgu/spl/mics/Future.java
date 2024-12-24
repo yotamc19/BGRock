63c156c7ff26db16d7719a824ee76ca0b60afd87
Jotam Combe <jotamc@wix.com>
2024-12-24 21:37:39 +0200

    future completed

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -11,56 +11,85 @@ import java.util.concurrent.TimeUnit;
  * No public constructor is allowed except for the empty constructor.
  */
 public class Future<T> {
-	
+	private T result;
+	private boolean isResolved;
+	private final Object lock;
+
 	/**
 	 * This should be the the only public constructor in this class.
 	 */
 	public Future() {
-		//TODO: implement this
-	}
-	
-	/**
-     * retrieves the result the Future object holds if it has been resolved.
-     * This is a blocking method! It waits for the computation in case it has
-     * not been completed.
-     * <p>
-     * @return return the result of type T if it is available, if not wait until it is available.
-     * 	       
-     */
-	public T get() {
-		//TODO: implement this.
-		return null;
-	}
-	
-	/**
-     * Resolves the result of this Future object.
-     */
-	public void resolve (T result) {
-		//TODO: implement this.
-	}
-	
-	/**
-     * @return true if this object has been resolved, false otherwise
-     */
-	public boolean isDone() {
-		//TODO: implement this.
-		return false;
-	}
-	
-	/**
-     * retrieves the result the Future object holds if it has been resolved,
-     * This method is non-blocking, it has a limited amount of time determined
-     * by {@code timeout}
-     * <p>
-     * @param timout 	the maximal amount of time units to wait for the result.
-     * @param unit		the {@link TimeUnit} time units to wait.
-     * @return return the result of type T if it is available, if not, 
-     * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
-     *         elapsed, return null.
-     */
-	public T get(long timeout, TimeUnit unit) {
-		//TODO: implement this.
-		return null;
+		isResolved = false;
+		lock = new Object();
 	}
 
+	/**
+	 * retrieves the result the Future object holds if it has been resolved.
+	 * This is a blocking method! It waits for the computation in case it has
+	 * not been completed.
+	 * <p>
+	 * 
+	 * @return return the result of type T if it is available, if not wait until it
+	 *         is available.
+	 * 
+	 */
+	public T get() {
+		synchronized (lock) {
+			while (!isResolved) {
+				try {
+					lock.wait();
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
+			}
+			return result;
+		}
+	}
+
+	/**
+	 * Resolves the result of this Future object.
+	 */
+	public void resolve(T result) {
+		synchronized (lock) {
+			if (!isResolved) {
+				this.result = result;
+				isResolved = true;
+				lock.notifyAll();
+			}
+		}
+	}
+
+	/**
+	 * @return true if this object has been resolved, false otherwise
+	 */
+	public boolean isDone() {
+		return isResolved;
+	}
+
+	/**
+	 * retrieves the result the Future object holds if it has been resolved,
+	 * This method is non-blocking, it has a limited amount of time determined
+	 * by {@code timeout}
+	 * <p>
+	 * 
+	 * @param timout the maximal amount of time units to wait for the result.
+	 * @param unit   the {@link TimeUnit} time units to wait.
+	 * @return return the result of type T if it is available, if not,
+	 *         wait for {@code timeout} TimeUnits {@code unit}. If time has
+	 *         elapsed, return null.
+	 */
+	public T get(long timeout, TimeUnit unit) {
+		long timeoutInMl = unit.toMillis(timeout);
+		
+		synchronized (lock) {
+			if (!isResolved) {
+				try {
+					lock.wait(timeoutInMl);
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
+			}
+			return isResolved ? result : null;
+		}
+	}
 }
