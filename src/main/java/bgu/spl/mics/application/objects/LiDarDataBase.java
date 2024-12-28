ec7f111903d0910740ecd95360920922d9f7fc61
Jotam Combe <jotamc@wix.com>
2024-12-28 12:45:01 +0200

    lidar db added

--- src/main/java/bgu/spl/mics/application/objects/LiDarDataBase.java
+++ src/main/java/bgu/spl/mics/application/objects/LiDarDataBase.java
@@ -1,10 +1,24 @@
 package bgu.spl.mics.application.objects;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 /**
  * LiDarDataBase is a singleton class responsible for managing LiDAR data.
  * It provides access to cloud point data and other relevant information for tracked objects.
  */
 public class LiDarDataBase {
+    private static LiDarDataBase instance = null;
+    private List<StampedCloudPoints> cloudPoints;
+
+    private LiDarDataBase() {
+        cloudPoints = new ArrayList<>();
+    }
 
     /**
      * Returns the singleton instance of LiDarDataBase.
@@ -13,7 +27,27 @@ public class LiDarDataBase {
      * @return The singleton instance of LiDarDataBase.
      */
     public static LiDarDataBase getInstance(String filePath) {
-        // TODO: Implement this
-        return null;
+        if (instance == null) {
+            instance = new LiDarDataBase();
+            instance.loadData(filePath);
+        }
+        return instance;
+    }
+
+    public List<StampedCloudPoints> getCloudPoints() {
+        return cloudPoints;
+    }
+
+    public void loadData(String filePath) {
+        Gson gson = new Gson();
+        try {
+            FileReader reader = new FileReader(filePath);
+            Type stampedCloudPointsType = new TypeToken<List<StampedCloudPoints>>() {
+            }.getType();
+            cloudPoints = gson.fromJson(reader, stampedCloudPointsType);
+        } catch (IOException e) {
+            e.printStackTrace();
+            cloudPoints = null;
+        }
     }
 }
