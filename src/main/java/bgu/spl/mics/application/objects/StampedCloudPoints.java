70e1e542831b6f5cbc1377d908f070a288b2f86a
Jotam Combe <jotamc@wix.com>
2024-12-28 12:44:40 +0200

    stamped cloud points created

--- src/main/java/bgu/spl/mics/application/objects/StampedCloudPoints.java
+++ src/main/java/bgu/spl/mics/application/objects/StampedCloudPoints.java
@@ -1,9 +1,31 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.List;
+
 /**
  * Represents a group of cloud points corresponding to a specific timestamp.
  * Used by the LiDAR system to store and process point cloud data for tracked objects.
  */
 public class StampedCloudPoints {
-    // TODO: Define fields and methods.
+    private final String id;
+    private final int time;
+    private final List<double[]> cloudPoints;
+
+    public StampedCloudPoints(String id, int time, List<double[]> cloudPoints) {
+        this.id = id;
+        this.time = time;
+        this.cloudPoints = cloudPoints;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public List<double[]> getCloudPoints() {
+        return cloudPoints;
+    }
 }
